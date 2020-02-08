cbe420d0a84081097d0915a99ca34a93d601ce07
ktsyr1 <46694587+ktsyr1@users.noreply.github.com>
2020-02-08 23:54:48 +0200

    Update db.js

--- bin/db.js
+++ bin/db.js
@@ -1,17 +1,28 @@
-var mongoose = require('mongoose');
-const uri = 'mongodb://localhost:27017/idna';
+// var mongoose = require('mongoose');
+// const uri = 'mongodb://localhost:27017/idna';
 
-mongoose.connect(uri, {
+// mongoose.connect(uri, {
 
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-}, (err) => {
-    if (err) {
-        console.log(err);
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true
+// }, (err) => {
+//     if (err) {
+//         console.log(err);
 
-    } else {
-        console.log('db connected');
+//     } else {
+//         console.log('db connected');
 
-    }
+//     }
+// });
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://ktsyr1:idlibtp123@cluster0-6xfyu.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true , useUnifiedTopology: true});
+client.connect(err => {
+  const collection = client.db("idna").collection("course");
+  // perform actions on the collection object
+  console.log('fs');
+  
+  client.close();
 });
